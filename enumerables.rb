8fdb47db580ce637d0dfc5063aa03d5884e6c763
IBTechRaj <raj_shk@rediffmail.com>
2019-04-18 20:27:19 +0530

    done

--- enumerables.rb
+++ enumerables.rb
@@ -87,33 +87,33 @@ end
 
 # Examples to test the above
 
-#arr = [2, 3, 2, 4, 3]
-#arr1 = %w[x i s]
-#arr2 = []
-#hash1 = { 'Jan' => 1, 'Feb' => 2, 'Mar' => 3 }
+# arr = [2, 3, 2, 4, 3]
+# arr1 = %w[x i s]
+# arr2 = []
+# hash1 = { 'Jan' => 1, 'Feb' => 2, 'Mar' => 3 }
 
-#arr.each { |x| puts x + 5 }
-#arr.my_each { |x| puts x * 3 }
+# arr.each { |x| puts x + 5 }
+# arr.my_each { |x| puts x * 3 }
 
-#[:foo, :bar, :baz].my_each_with_index do |value, index|
+# [:foo, :bar, :baz].my_each_with_index do |value, index|
 #    puts "#{index}: #{value}"
-#end
+# end
 
-#puts arr.my_select { |x| x >= 3 }
+# puts arr.my_select { |x| x >= 3 }
 
-#puts hash1.my_all? { |_k, v| v >= 1 }
-#puts arr.my_all? { |x| x > 2 }
+# puts hash1.my_all? { |_k, v| v >= 1 }
+# puts arr.my_all? { |x| x > 2 }
 
-#puts arr.my_any? { |x| x > 4 }
+# puts arr.my_any? { |x| x > 4 }
 
-#puts arr.my_none? { |x| x > 4 }
+# puts arr.my_none? { |x| x > 4 }
 
-#puts arr.my_count
-#puts arr.my_count(2)
-#puts arr.my_count(&:even?)
+# puts arr.my_count
+# puts arr.my_count(2)
+# puts arr.my_count(&:even?)
 
-#puts arr.my_inject { |final_val, x| final_val + x }
-#puts arr.my_inject (1) { |final_val, x| final_val * x }
+# puts arr.my_inject { |final_val, x| final_val + x }
+# puts arr.my_inject (1) { |final_val, x| final_val * x }
 
-#puts arr1.my_map { |n| n.upcase }
+# puts arr1.my_map { |n| n.upcase }
 
